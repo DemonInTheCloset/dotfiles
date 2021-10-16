f2e6aacb29ae5bba99390c1c42ab62ad99e75df0
DemonInTheCloset <demoninthecloset@gmail.com>
2021-10-16 11:02:30 +0200

    Reskinned prompt
    Moved Clock to the Left Prompt and virtual env to the right

--- dot_config/zsh/prompt.zsh
+++ dot_config/zsh/prompt.zsh
@@ -2,11 +2,11 @@
 prompt_ditc_setup() {
     # Left Prompt
     #             user        @ hostname                  ❯    (# if root)
-    PS1='%F{green}%n%f%F{blue}@%f%F{magenta}%m%f%F{yellow}%(1V, ,)❯%f%(!,#, )'
+    PS1='%F{green}%n%f%F{blue}@%f%F{magenta}%m%f %B%F{white}%*%f%b%F{yellow}❯%f%(!,#, )'
 
     # Right Prompt
-    #               ❮  program exit code   Hour:Minute:Sec  YYYY-mm-dd       current directory
-    RPS1='%F{yellow}❮%f%(?, ,%F{red}[%?]%f)%B%F{white}%*%f%b %F{cyan}%D{%F}%f %F{blue}%(4.,%-1~/../%1~,%~)%f'
+    #               ❮  program exit code   YYYY-mm-dd       current directory
+    RPS1='%F{yellow}❮%(1V, ,)%f%(?,,%F{red}[%?]%f)%F{cyan}%D{%F}%f %F{blue}%(4.,%-1~/../%1~,%~)%f'
 }
 
 # Add as a theme
