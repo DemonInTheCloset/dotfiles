c8dd8b99c9d864f7ee77879ac34f4ea0ee886229
DemonInTheCloset <demoninthecloset@gmail.com>
2021-10-20 16:43:14 +0200

    Fixed powerlevel10k prompt not working
    Forgot to commit config.zsh

--- dot_config/zsh/config.zsh
+++ dot_config/zsh/config.zsh
@@ -11,8 +11,11 @@ source "$ZDOTDIR/hooks.zsh"
 source "$ZDOTDIR/aliases.zsh"
 
 # Set prompt
-source "$ZDOTDIR/prompt.zsh"
-source "$ZDOTDIR/activate_prompt.zsh"
+# source "$ZDOTDIR/prompt.zsh"
+# source "$ZDOTDIR/activate_prompt.zsh"
+
+source /usr/share/zsh-theme-powerlevel10k/powerlevel10k.zsh-theme
+
 # prompt off
 # powerline-daemon -q
 # . /usr/share/powerline/bindings/zsh/powerline.zsh
