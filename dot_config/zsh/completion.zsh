d14eb655e82e5c64f87c6a6fb4a1001849bb351c
DemonInTheCloset <demoninthecloset@gmail.com>
2021-10-26 16:24:24 +0200

    feat(zsh): Improved completion options
    - Completing file extensions after *.
    - Case insensitive and partial words matching
    - Caching of completion results
    - Use colors in the completion menu

--- dot_config/zsh/completion.zsh
+++ dot_config/zsh/completion.zsh
@@ -3,6 +3,19 @@ compinit
 # Press tab twice to get suggestions
 zstyle ':completion:*' menu select
 
+# Completion style               file ext.   complete  correct spelling mistakes
+zstyle ':completion:*' completer _extensions _complete _approximate
+
+# Try case insensitive or partial words after failing match
+zstyle ':completion:*' matcher-list '' 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
+
+# Use caching
+zstyle ':completion:*' use-cache on
+zstyle ':completion:*' cache-path "$XDG_CACHE_HOME/zsh/.zcompcache"
+
+# Use ls Colors
+zstyle ':completion:*:default' list-colors ${(s.:.)LS_COLORS}
+
 # Command line options completions for aliases
 setopt COMPLETE_ALIASES
 
