be32087bbd67bebb8141d31201cfc6b15a178857
DemonInTheCloset <demoninthecloset@gmail.com>
2021-12-20 18:45:22 +0100

    feat: Setup script installs Packer automatically

--- .setup.sh
+++ .setup.sh
@@ -6,3 +6,6 @@ chezmoi apply 2> /tmp/chezmoi.log
 # Generate symlinks
 ln -sf "$XDG_CONFIG_HOME"/zsh/.zshenv "$HOME"/.zshenv
 ln -sf "$XDG_CONFIG_HOME"/X11/xresources "$HOME"/.Xresources
+
+# Download nvim-packer
+git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
