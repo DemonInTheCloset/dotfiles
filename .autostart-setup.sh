4d1303c8746296c5f772250273061b714689c0aa
DemonInTheCloset <demoninthecloset@gmail.com>
2021-12-22 13:19:27 +0100

    feat: Moved away from XDG autostart

---
+++ .autostart-setup.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+prg-autostart qutebrowser
+prg-autostart blueman-applet
+prg-autostart pasystray
+prg-autostart start-pulseaudio-x11
+prg-autostart keepassxc
+prg-autostart picom -b
+prg-autostart signal-desktop --start-in-tray
+prg-autostart thunderbird
+prg-autostart xwallpaper --zoom /usr/share/backgrounds/archlinux/awesome.png
