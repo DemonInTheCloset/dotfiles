b882f36b31fffc2cbfe5b375d8cf25761dbd990c
DemonInTheCloset <demoninthecloset@gmail.com>
2022-01-24 11:35:32 +0100

    feat(nvim): Cleaned up augroups

--- dot_config/nvim/lua/user/augroups.lua
+++ dot_config/nvim/lua/user/augroups.lua
@@ -1,58 +1,35 @@
-vim.cmd([[
-" augroups
-augroup OpenFolds " Open folds when opening a file
-    " silent! makes it so that it ignores the error: No Folds Found
-    autocmd BufRead * silent! %foldopen!
-augroup END
+local function define_augroup(name, autocmds)
+	vim.cmd("augroup " .. name)
+	vim.cmd("autocmd!")
+    autocmds()
+	vim.cmd("augroup END")
+end
 
-augroup FTApperance " Change default tab settings for specific files
-    " C tab settings (Tabs are 8 spaces and don't expand tabs to spaces)
-    autocmd FileType c setlocal tabstop=8
-    autocmd FileType c setlocal shiftwidth=8
-    autocmd FileType c setlocal noexpandtab
-    " Haskell tab settings (Tabs are 2 spaces)
-    autocmd FileType haskell setlocal tabstop=2
-    autocmd FileType haskell setlocal shiftwidth=2
-    " Markdown tab settings (Tabs are 2 spaces)
-    autocmd FileType markdown setlocal tabstop=2
-    autocmd FileType markdown setlocal shiftwidth=2
-    autocmd FileType markdown setlocal wrap
-augroup END
+define_augroup("OpenFolds", function()
+	vim.cmd("autocmd BufRead * silent! %foldopen!")
+end)
 
-augroup TerminalMode " Settings for vim terminal mode
-    autocmd TermOpen * setlocal nonumber norelativenumber
-augroup END
+define_augroup("FTApperance", function()
+	vim.cmd("autocmd FileType c setlocal tabstop=8 shiftwidth=8 noexpandtab")
+	vim.cmd("autocmd FileType haskell setlocal tabstop=2 shiftwidth=2")
+	vim.cmd("autocmd FileType markdown setlocal tabstop=2 shiftwidth=2 textwidth=80")
+	vim.cmd("autocmd TermOpen * setlocal nonumber norelativenumber")
+end)
 
-" augroup SymbolHighlight " Highlight all symbols with the same name
-"     autocmd CursorHold * silent call CocActionAsync('highlight')
-" augroup END
+define_augroup("DispatchCompiler", function()
+	vim.cmd("autocmd FileType java let b:dispatch = 'gradle build'")
+	vim.cmd("autocmd FileType python let b:dispatch = 'mypy %'")
+	vim.cmd("autocmd FileType markdown let b:dispatch = 'pandoc --pdf-engine=xelatex -f markdown % -o %.pdf'")
+	vim.cmd("autocmd FileType tex let b:dispatch = 'latexmk -pvc -pdflua %'")
+	vim.cmd("autocmd FileType plaintex let b:dispatch = 'latexmk -pvc -pdf %'")
+end)
 
-augroup Templates " File templates when creating files
-    autocmd BufNewFile *.py  0r ~/.config/nvim/templates/skeleton.py
-    autocmd BufNewFile *.zsh 0r ~/.config/nvim/templates/skeleton.zsh
-augroup END
+define_augroup("TemplateFiles", function()
+	vim.cmd("autocmd BufNewFile *.py  0r ~/.config/nvim/templates/skeleton.py")
+	vim.cmd("autocmd BufNewFile *.zsh 0r ~/.config/nvim/templates/skeleton.zsh")
+end)
 
-" Auto format files
-augroup RemoveTrailingWhitespace
-    autocmd BufWritePre *.md :%s/\s\+$//e
-    autocmd BufWritePre *.py :%s/\s\+$//e
-augroup END
-
-function! ClangFormat()
-    " let l:formatdiff = 1
-    py3f /usr/share/clang/clang-format.py
-endfunction
-
-" augroup RunFormatter " run formaters when writting to disk
-"     autocmd BufWritePre *.py :Black
-"     autocmd BufWritePre *.c call ClangFormat()
-" augroup END
-
-augroup SetCompiler " set compiler to run with vim-dispatch
-    autocmd FileType java let b:dispatch = 'gradle build'
-    autocmd FileType python let b:dispatch = 'mypy %'
-    autocmd FileType markdown let b:dispatch = 'pandoc --pdf-engine=xelatex -f markdown % -o %.pdf'
-    autocmd FileType tex let b:dispatch = 'latexmk -pvc -pdf %'
-    autocmd FileType plaintex let b:dispatch = 'latexmk -pvc -pdf %'
-augroup END
-]])
+define_augroup("TrimWhiteSpace", function ()
+    vim.cmd("autocmd BufWritePre *.md :%s/\\s\\+$//e")
+    vim.cmd("autocmd BufWritePre *.py :%s/\\s\\+$//e")
+end)
