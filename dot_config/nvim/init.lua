8f0b09ad2aa78cc7ae86b9501354a9d04f4f25b7
DemonInTheCloset <demoninthecloset@gmail.com>
2022-01-24 11:34:38 +0100

    fix(nvim): Made config sourcing more explicit

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,23 +1,20 @@
 -- Enable Filetype Plugin
 vim.cmd("filetype plugin on")
 
-local config_files = {
-    "user/plugins",
-    "user/settings",
-    "user/cmp",
-    "user/treesitter",
-    "user/lsp-config",
-    "user/statusline",
-    "user/augroups",
-    "user/telescope",
-    "user/plugconf",
-    -- 'error_file', -- test file to make sure errors are non fatal
-}
-
--- Source configuration
-for _, file in ipairs(config_files) do
+-- Continue reading configuration even if it contains an error
+local function source_config(file)
     local ok, _ = pcall(require, file)
     if not ok then
         print("Error in config file: " .. file)
     end
 end
+
+source_config("user/plugins")
+source_config("user/settings")
+source_config("user/cmp")
+source_config("user/treesitter")
+source_config("user/lsp-config")
+source_config("user/statusline")
+source_config("user/augroups")
+source_config("user/telescope")
+source_config("user/plugconf")
