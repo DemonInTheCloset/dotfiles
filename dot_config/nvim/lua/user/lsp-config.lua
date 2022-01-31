70e5338d1b424095594fee2efd03290c61476d1e
DemonInTheCloset <demoninthecloset@gmail.com>
2022-01-31 22:53:49 +0100

    feat(nvim): Added LSP Keymaps

--- dot_config/nvim/lua/user/lsp-config.lua
+++ dot_config/nvim/lua/user/lsp-config.lua
@@ -3,7 +3,28 @@ table.insert(runtime_path, "lua/?.lua")
 table.insert(runtime_path, "lua/?/init.lua")
 
 local function on_attach(_, bufnr)
+	local opts = { noremap = true }
+	-- Omnifunc
 	vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
+
+	-- Keymaps
+    -- LSP actions
+	vim.api.nvim_buf_set_keymap(bufnr, "n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
+	vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>r", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
+	vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>A", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
+
+    -- LSP goto ...
+	vim.api.nvim_buf_set_keymap(bufnr, "n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
+	vim.api.nvim_buf_set_keymap(bufnr, "n", "gt", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
+	vim.api.nvim_buf_set_keymap(bufnr, "n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
+
+    -- Diagnostics
+	vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>dj", "<cmd>lua vim.diagnostic.goto_next()<CR>", opts)
+	vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>dk", "<cmd>lua vim.diagnostic.goto_prev()<CR>", opts)
+
+    -- Telescope
+    vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>td", "<cmd>Telescope diagnostics<CR>", opts)
+    vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>ta", "<cmd>Telescope lsp_code_actions<CR>", opts)
 end
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
@@ -36,8 +57,8 @@ lspconfig["pyright"].setup({
 	capabilities = capabilities,
 })
 lspconfig["remark_ls"].setup({
-    on_attach = on_attach,
-    capabilities = capabilities,
+	on_attach = on_attach,
+	capabilities = capabilities,
 })
 lspconfig["rust_analyzer"].setup({
 	on_attach = on_attach,
