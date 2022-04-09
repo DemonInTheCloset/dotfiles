8999bba224fdbe20d9e00bd03fef9864cdba5b83
DemonInTheCloset <demoninthecloset@gmail.com>
2022-04-09 13:33:23 +0200

    fix(nvim): Disable snippets while I fix them

--- home/dot_config/nvim/lua/user/snippets.lua
+++ home/dot_config/nvim/lua/user/snippets.lua
@@ -1,11 +1,11 @@
 local luasnip = require "luasnip"
 
-local fmt = require("luasnip.extras.fmt").fmt
-local i = luasnip.insert_node
-local rep = require("luasnip.extras").rep
+-- local fmt = require("luasnip.extras.fmt").fmt
+-- local i = luasnip.insert_node
+-- local rep = require("luasnip.extras").rep
 
-luasnip.snippets.tex = {
-	luasnip.s("begin", fmt("\\begin{{{}}}\n{}\n\\end{{{}}}", { i(1), i(2), rep(1) })),
-}
+-- luasnip.snippets.tex = {
+-- 	luasnip.s("begin", fmt("\\begin{{{}}}\n{}\n\\end{{{}}}", { i(1), i(2), rep(1) })),
+-- }
 
 return luasnip
