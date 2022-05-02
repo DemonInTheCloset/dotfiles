57d3368e0836c92b67ed95f9f1dd2ab7aadb0819
DemonInTheCloset <demoninthecloset@gmail.com>
2022-05-02 16:49:11 +0200

    fix(nvim): Using 0.7 features when formatting

--- home/dot_config/nvim/init.lua
+++ home/dot_config/nvim/init.lua
@@ -272,6 +272,8 @@ colorizer_conf["*"] = { names = false, RRGGBBAA = true }
 prequire("colorizer").setup(colorizer_conf)
 
 -- [[ null_ls ]] --
+local lsp_formatting = vim.api.nvim_create_augroup("LspFormatting", {})
+
 local null_ls = prequire "null-ls"
 null_ls.setup {
 	sources = {
@@ -301,14 +303,17 @@ null_ls.setup {
 		null_ls.builtins.code_actions.shellcheck,
 	},
 	-- Format On Save
-	on_attach = function(client)
-		if client.resolved_capabilities.document_formatting then
-			vim.cmd [[
-            augroup LspFormatting
-                autocmd! * <buffer>
-                autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_seq_sync()
-            augroup END
-            ]]
+	on_attach = function(client, bufnr)
+		if client.supports_method "textDocument/formatting" then
+			vim.api.nvim_clear_autocmds { group = lsp_formatting, buffer = bufnr }
+			vim.api.nvim_create_autocmd(
+				"BufWritePre",
+				{
+					group = lsp_formatting,
+					buffer = bufnr,
+					callback = vim.lsp.buf.formatting_seq_sync,
+				}
+			)
 		end
 	end,
 }
