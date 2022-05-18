641fa83d31231a10429313061363f8d9d8991192
DemonInTheCloset <demoninthecloset@gmail.com>
2022-05-18 20:19:20 +0200

    feat: Awesome theme working

--- home/dot_config/nvim/init.lua
+++ home/dot_config/nvim/init.lua
@@ -180,7 +180,18 @@ local user_lspconfig = prequire "user.lspconfig"
 
 -- [[ plugins ]] --
 local colorizer = prequire "colorizer"
-colorizer.setup({}, { names = false, RRGGBBAA = true })
+colorizer.setup({ "*" }, {
+	RGB = true, -- #RGB hex codes
+	RRGGBB = true, -- #RRGGBB hex codes
+	names = false, -- "Name" codes like Blue
+	RRGGBBAA = true, -- #RRGGBBAA hex codes
+	rgb_fn = false, -- CSS rgb() and rgba() functions
+	hsl_fn = false, -- CSS hsl() and hsla() functions
+	css = false, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+	css_fn = false, -- Enable all CSS *functions*: rgb_fn, hsl_fn
+	-- Available modes: foreground, background
+	mode = "background", -- Set the display mode.
+})
 
 -- [[ rust-tools ]]
 local rust_tools = prequire "rust-tools"
