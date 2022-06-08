c580e7b921f3385eebc37d20ab598e6248b53e3d
DemonInTheCloset <demoninthecloset@gmail.com>
2022-06-08 21:13:23 +0200

    fix(awesome): Brightness widget edge case
    Show ?? for brightness when the information isn't available

--- home/dot_config/awesome/widgets/brightness.lua
+++ home/dot_config/awesome/widgets/brightness.lua
@@ -1,5 +1,9 @@
 local awful = require("awful")
 
-return awful.widget.watch("xbacklight -perceived -get", 10, function(widget, stdout)
+return awful.widget.watch("xbacklight -perceived -get", 10, function(widget, stdout, stderr)
+	if stderr and stderr ~= "" then
+		widget:set_text(" ??")
+		return
+	end
 	widget:set_text(" " .. stdout:match("%d+") .. "%")
 end)
