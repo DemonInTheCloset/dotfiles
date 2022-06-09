3b38ec8e623e66bea77f9c48da7e7db232d9a585
DemonInTheCloset <demoninthecloset@gmail.com>
2022-06-09 14:17:17 +0200

    fix(awesome): Slightly reshaped battery widget

--- home/dot_config/awesome/widgets/battery.lua
+++ home/dot_config/awesome/widgets/battery.lua
@@ -3,33 +3,26 @@ local vicious = require("vicious")
 local gears = require("gears")
 local wibox = require("wibox")
 
-local batbox = wibox.layout.margin(
-	wibox.widget({
-		{
-			max_value = 1,
-			widget = wibox.widget.progressbar,
-			border_width = 0.5,
-			shape = gears.shape.rounded_bar,
-			border_color = "#282828",
-			color = {
-				type = "linear",
-				from = { 0, 0 },
-				to = { 0, 10 },
-				--stops = { { 0, "#fb4934" }, { 1, "#83c07c" } },
-				stops = { { 0, "#83c07c" }, { 1, "#fb4934" } },
-			},
+local batbox = wibox.widget({
+	{
+		max_value = 1,
+		widget = wibox.widget.progressbar,
+		shape = gears.shape.rounded_bar,
+		border_width = 0.5,
+		border_color = "#3c3836",
+		color = {
+			type = "linear",
+			from = { 0, 0 },
+			to = { 0, 30 },
+			stops = { { 0, "#b8bb26" }, { 0.5, "#83c07c" }, { 1, "#fb4934" } },
 		},
-		forced_height = 10,
-		forced_width = 8,
-		direction = "east",
-		color = beautiful.fg_widget,
-		layout = wibox.container.rotate,
-	}),
-	1,
-	1,
-	3,
-	3
-)
+	},
+	forced_height = 10,
+	forced_width = 8,
+	direction = "east",
+	color = beautiful.fg_widget,
+	layout = wibox.container.rotate,
+})
 
 vicious.register(batbox, vicious.widgets.bat, "$2", 61, "BAT0")
 
