41ac5dd457e899c5562a684f865a0ef42011a7a1
DemonInTheCloset <demoninthecloset@gmail.com>
2022-06-10 08:38:21 +0200

    fix(awesome): Simplified volume widget

--- home/dot_config/awesome/widgets/volume.lua
+++ home/dot_config/awesome/widgets/volume.lua
@@ -1,12 +1,12 @@
 local awful = require("awful")
 
-return awful.widget.watch("sh -c 'echo \"$(pamixer --get-volume) $(pamixer --get-mute)\"'", 10, function(widget, stdout)
-    local volume = stdout:match("%d+")
-    local mute = stdout:match("%a+")
+return awful.widget.watch("pamixer --get-volume pamixer --get-mute", 10, function(widget, stdout)
+	local volume = stdout:match("%d+")
+	local mute = stdout:match("%a+")
 
-    if mute == "true" or volume == "0" then
-        widget:set_text("婢 " .. volume .. "%")
-    else
-        widget:set_text("墳 " .. volume .. "%")
-    end
+	if mute == "true" or volume == "0" then
+		widget:set_text("婢 " .. volume .. "%")
+	else
+		widget:set_text("墳 " .. volume .. "%")
+	end
 end)
