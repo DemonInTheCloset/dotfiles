4e53ca7a818b1f8fa0df88bb910e776df4478a0a
DemonInTheCloset <demoninthecloset@gmail.com>
2022-06-19 14:04:10 +0200

    feat(awesome): Rounded window corners

--- home/dot_config/awesome/rc.lua
+++ home/dot_config/awesome/rc.lua
@@ -256,6 +256,9 @@ awful.rules.rules = {
 -- {{{ Signals
 -- Signal function to execute when a new client appears.
 client.connect_signal("manage", function(c)
+	c.shape = function(cr, w, h)
+		gears.shape.rounded_rect(cr, w, h, 8)
+	end
 	-- Set the windows at the slave,
 	-- i.e. put it at the end of others instead of setting it master.
 	-- if not awesome.startup then awful.client.setslave(c) end
