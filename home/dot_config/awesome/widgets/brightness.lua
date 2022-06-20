04a6fd60a2df8fb2b0a688521703aca5ee8daad7
DemonInTheCloset <demoninthecloset@gmail.com>
2022-06-20 16:19:16 +0200

    feat(awesome): Updated brightness widget

--- home/dot_config/awesome/widgets/brightness.lua
+++ home/dot_config/awesome/widgets/brightness.lua
@@ -1,9 +1,11 @@
 local awful = require("awful")
 
 return awful.widget.watch("xbacklight -perceived -get", 10, function(widget, stdout, stderr)
+	-- No backlight
 	if stderr and stderr ~= "" then
-		widget:set_text(" ??")
+		widget:set_text("")
 		return
 	end
+
 	widget:set_text(" " .. stdout:match("%d+") .. "%")
 end)
