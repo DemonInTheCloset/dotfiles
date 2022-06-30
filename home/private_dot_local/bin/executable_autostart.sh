8322e250783dc601e0c87b92f91de91ff0e1f609
DemonInTheCloset <demoninthecloset@gmail.com>
2022-06-30 16:22:29 +0200

    fix(autostart): Getting applications to start

--- home/private_dot_local/bin/executable_autostart.sh
+++ home/private_dot_local/bin/executable_autostart.sh
@@ -8,7 +8,7 @@ signal-desktop --start-in-tray >>"$logdir"/signal.log &
 # activitywatch
 /usr/bin/aw-watcher-afk >>"$logdir"/aw-watcher-afk.log &
 /usr/bin/aw-watcher-window >>"$logdir"/aw-watcher-window.log &
-/usr/bin/aw-qt --autostart-modules none >>"$logdir"/aw-qt.log &
+# /usr/bin/aw-qt --autostart-modules none >>"$logdir"/aw-qt.log &
 
 # audio
 /usr/bin/pipewire >>"$logdir"/pipewire.log &
@@ -19,7 +19,7 @@ signal-desktop --start-in-tray >>"$logdir"/signal.log &
 /usr/bin/blueman-applet >>"$logdir"/blueman-applet.log &
 
 # dunst
-/usr/bin/dunst -verbosity info --startup_notification true >>"$logdir"/dunst.log &
+# /usr/bin/dunst -verbosity info --startup_notification true >>"$logdir"/dunst.log &
 
 # mailnotify
 /usr/bin/mbsync -V -a -c "$XDG_CONFIG_HOME"/isync/mbsyncrc >>"$logdir"/mbsync.log &
