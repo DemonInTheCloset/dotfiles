cafeaa2817d5e887db7040b7aaa9990512790966
DemonInTheCloset <demoninthecloset@gmail.com>
2022-07-08 10:12:28 +0200

    feat(awesome): Moved to it's own repo

--- home/dot_config/awesome/widgets/launcher.lua
+++
@@ -1,6 +0,0 @@
-local beautiful = require 'beautiful'
-local awful = require 'awful'
-
-local mainmenu = require 'widgets/mainmenu'
-
-return awful.widget.launcher { image = beautiful.awesome_icon, menu = mainmenu }
--- home/dot_config/awesome/widgets/textclock.lua
+++
@@ -1,5 +0,0 @@
-local wibox = require 'wibox'
-
--- {{{ Wibar
--- Create a textclock widget
-return wibox.widget.textclock '%F %H:%M'
