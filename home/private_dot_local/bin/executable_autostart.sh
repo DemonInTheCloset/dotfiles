4b038903e4ac74bb0b768fdd2432db1f922108b6
DemonInTheCloset <demoninthecloset@gmail.com>
2022-07-08 14:02:14 +0200

    fix: Removing duplicate program autostart

--- home/private_dot_local/bin/executable_autostart.sh
+++ home/private_dot_local/bin/executable_autostart.sh
@@ -2,36 +2,8 @@
 
 logdir="$XDG_DATA_HOME"/dinit
 
-# Signal
-signal-desktop --start-in-tray >>"$logdir"/signal.log &
-
-# activitywatch
-/usr/bin/aw-watcher-afk >>"$logdir"/aw-watcher-afk.log &
-/usr/bin/aw-watcher-window >>"$logdir"/aw-watcher-window.log &
-# /usr/bin/aw-qt --autostart-modules none >>"$logdir"/aw-qt.log &
-
-# audio
-/usr/bin/pipewire >>"$logdir"/pipewire.log &
-/usr/bin/wireplumber >>"$logdir"/wireplumber.log &
-/usr/bin/pipewire-pulse >>"$logdir"/pipewire-pulse.log &
-
-# blueman-applet
-# /usr/bin/blueman-applet >>"$logdir"/blueman-applet.log &
-
-# dunst
-# /usr/bin/dunst -verbosity info --startup_notification true >>"$logdir"/dunst.log &
-
 # picom
 /usr/bin/picom >>"$logdir"/picom.log &
 
-# sxhkd
-# /usr/bin/sxhkd >>"$logdir"/sxhkd.log &
-
 # xwallpaper
 /usr/bin/xwallpaper --zoom /usr/share/backgrounds/archlinux/awesome.png >>"$logdir"/xwallpaper.log &
-
-# mailnotify
-/usr/bin/mbsync -V -a -c "$XDG_CONFIG_HOME"/isync/mbsyncrc >>"$logdir"/mbsync.log &
-for file in "$XDG_CONFIG_HOME"/imapnotify/*.conf; do
-	/usr/bin/goimapnotify -conf "$file" >>"$logdir/$(basename -s .conf "$file")".log &
-done
