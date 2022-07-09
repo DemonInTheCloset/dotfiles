8cbd02bbc08efd660a3150b0b821b4b1082b97e3
DemonInTheCloset <demoninthecloset@gmail.com>
2022-07-09 13:43:48 +0200

    feat(wezterm): Enabled multiplexing

--- home/dot_config/wezterm/wezterm.lua
+++ home/dot_config/wezterm/wezterm.lua
@@ -7,10 +7,17 @@ return {
 	hide_tab_bar_if_only_one_tab = true,
 	tab_bar_at_bottom = true,
 	-- -- Window
-	window_background_image = "/usr/share/backgrounds/archlinux/wave.png",
+	-- window_background_image = "/usr/share/backgrounds/archlinux/wave.png",
+	window_background_opacity = 0.8,
 	-- -- Color Scheme
 	color_scheme = "Gruvbox Dark",
 	-- Font
 	font = wezterm.font("Iosevka Term Extended", { weight = "Medium" }),
 	font_size = 11.0,
+	-- Multiplexing
+	unix_domains = {
+		{ name = "unix" },
+	},
+	-- Default Arguments (connect to multiplexing session)
+	default_gui_startup_args = { "connect", "unix" },
 }
